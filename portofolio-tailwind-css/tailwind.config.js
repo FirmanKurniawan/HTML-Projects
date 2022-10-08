97ad96e2fc89ad9517d0354544dd0b382d819c1b
Akhmad Faiz Abdulloh <akhmadfaizabdulloh@gmail.com>
2022-10-09 02:49:35 +0800

    add portofolio-tailwind-css

---
+++ portofolio-tailwind-css/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
